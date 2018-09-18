f7c65260da982557acd3c5e28e05e8dbdeb2a34f
Santiago Lopez <santiago.lopez.497@gmail.com>
2018-09-17 23:17:01 -0300

    Diccionario provisorio de errores

---
+++ Error.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#include "Error.hpp"
+
+void PrintError (status_t &st) {
+	static string error [MAX_ERROR_STR] = {
+		MSG_NO_ERROR,
+		MSG_ERROR_NULL_PTR,
+		MSG_ERROR_FILE_CORRUPTED,
+		MSG_ERROR_NO_MEMORY
+	};
+
+	if (st != ST_OK)
+		cerr << MSG_ERROR_PREFIX << ": " << error[st] << endl;
+}
